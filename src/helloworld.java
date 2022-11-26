006864d7cd8c82b273e954369b16b7b61654b643
Nguyenloc87521 <117912099+Nguyenloc87521@users.noreply.github.com>
2022-11-26 12:13:32 +0700

    java1

---
+++ src/helloworld.java
@@ -0,0 +1,28 @@
+public class helloworld {
+    public static void main(String[] args) {
+//câu1
+        System.out.println("Xin chào các bạn ");
+//câu2
+        String firstName;
+        String lastName;
+        firstName = "Lộc";
+         lastName = "Nguyễn";
+        System.out.println("Xin chào "+ firstName + lastName);
+
+
+//câu3
+        int a = 5;
+        int b = 15;
+        int sum;
+
+        sum = a + b;
+
+        System.out.println("Tổng của hai số là: "+sum);
+// câu 4
+        System.out.println(Math.pow(4,2));
+//câu5
+
+//câu 6
+
+    }
+}
